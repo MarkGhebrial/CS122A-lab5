3de72440d2fdecec3ed47b560eac590e76e22fdb
MarkGhebrial <mghebrialjr@icloud.com>
2026-04-30 18:31:26 -0700

    Add lpf and pcf files

---
+++ src/lcd_controller.sv
@@ -0,0 +1,14 @@
+module top
+(
+  input  CLK, //FPGA's clocck
+
+	output LCD_CLK,//LCD clock. 
+	output LCD_DEN,
+	output [4:0] LCD_R,
+	output [5:0] LCD_G,
+	output [4:0] LCD_B,
+);
+
+// TODO
+
+endmodule 
